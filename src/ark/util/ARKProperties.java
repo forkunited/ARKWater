034f05dced6a215e7b8e4ae86fc26d99f2225c86
Bill McDowell <forkunited@gmail.com>
2013-10-31 14:44:31 -0400

    Initial commit

---
+++ src/ark/util/ARKProperties.java
@@ -0,0 +1,32 @@
+package ark.util;
+
+import java.io.BufferedReader;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+public abstract class ARKProperties {
+	protected Properties properties = null;
+	protected Map<String, String> env = null;
+	
+	public ARKProperties(String[] possiblePaths) {
+		try {
+			this.properties = new Properties();
+			BufferedReader reader = FileUtil.getPropertiesReader(possiblePaths);
+			this.properties.load(reader);
+			reader.close();
+			this.env = System.getenv();
+			
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected String loadProperty(String property) {
+		String propertyValue = this.properties.getProperty(property);
+		for (Entry<String, String> envEntry : this.env.entrySet())
+			propertyValue = propertyValue.replace("${" + envEntry.getKey() + "}", envEntry.getValue());
+		return propertyValue;
+	}
+}
---
+++ src/ark/util/FileUtil.java
@@ -0,0 +1,25 @@
+package ark.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class FileUtil {
+	public static BufferedReader getFileReader(String path) {
+		File localFile = new File(path);
+		try {
+			if (localFile.exists())
+				return new BufferedReader(new FileReader(localFile));
+		} catch (Exception e) { }
+		return HadoopUtil.getFileReader(path);
+	}
+	
+	public static BufferedReader getPropertiesReader(String[] possiblePaths) {
+		for (int i = 0; i < possiblePaths.length; i++) {
+			BufferedReader r = getFileReader(possiblePaths[i]);
+			if (r != null)
+				return r;
+		}
+		return null;
+	}
+}
