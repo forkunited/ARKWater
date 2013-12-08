0bf7666ab18012a52d4a78732df8d80277007c58
Bill McDowell <forkunited@gmail.com>
2013-12-08 16:18:54 -0500

    Added path hack to file util

--- src/ark/util/FileUtil.java
+++ src/ark/util/FileUtil.java
@@ -10,6 +10,9 @@ public class FileUtil {
 		try {
 			if (localFile.exists())
 				return new BufferedReader(new FileReader(localFile));
+			localFile = new File(path.replace("/user/", "/home/")); // FIXME: This is a hack. Tries variations on path on occ-y
+			if (localFile.exists())
+				return new BufferedReader(new FileReader(localFile));
 		} catch (Exception e) { }
 		return HadoopUtil.getFileReader(path);
 	}
